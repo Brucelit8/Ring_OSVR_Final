835ca5745f429a19627f500df6c84f49fb3b4386
Galand Simon <simon.galand@gmail.com>
2016-11-29 18:01:40 +0100

    Push de la map
    Loule

---
+++ Ring_OSVR_Final/Assets/Script/FootSteps.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSteps : MonoBehaviour {
+
+    public Transform T;
+
+    private Vector3 pos;
+    private bool foot;
+    float timeCurrent;
+    public AudioSource fs1;
+    public  AudioSource fs2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pos = T.position;
+        foot = false;
+        timeCurrent = Time.fixedTime;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Time.fixedTime - timeCurrent > 0.4f)
+        {
+            if (pos != T.position)
+            {
+                if(foot)
+                {
+                    Debug.Log("fs1");
+                    fs1.Play();
+                }
+                else
+                {
+                    Debug.Log("fs2");
+                    fs2.Play();
+                }
+                foot = !foot;
+            }
+            timeCurrent = Time.fixedTime;
+            pos = T.position;
+        }
+	}
+}
---
+++ Ring_OSVR_Final/Assets/moveBoat.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBoat : MonoBehaviour {
+
+    float time = 0.0f;
+    float timer = 30.0f;
+    float speed = 0.01f;
+    public GameObject p1, p2;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time <= timer)
+        {
+            Vector3 t = new Vector3(Mathf.Lerp(p1.transform.position.x, p2.transform.position.x, time/ timer),
+                Mathf.Lerp(p1.transform.position.y, p2.transform.position.y, time/ timer),
+                Mathf.Lerp(p1.transform.position.z, p2.transform.position.z, time/ timer));
+            transform.position = t;
+            time += 0.01f;
+        }
+        else
+        {
+            time = 0.0f;
+            transform.position = p1.transform.position;
+        }
+
+    }
+}
